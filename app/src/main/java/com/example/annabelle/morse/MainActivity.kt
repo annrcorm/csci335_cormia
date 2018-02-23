04d59fd48fdff5cfb03cdc37bbf7c95b5ab016d0
annrcorm <annrcorm@ut.utm.edu>
2018-02-23 16:22:47 -0600

    .

--- app/src/main/java/com/example/annabelle/morse/MainActivity.kt
+++ app/src/main/java/com/example/annabelle/morse/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.annabelle.morse
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.text.method.ScrollingMovementMethod
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +26,39 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+
+        mTextView.movementMethod = ScrollingMovementMethod()
+
+        testButton.setOnClickListener { view ->
+            appendTextAndScroll(inputText.text.toString())
+            hideKeyboard()
+        }
+
     }
+
+    private fun appendTextAndScroll(text: String) {
+        if (mTextView != null) {
+            mTextView.append(text + "\n")
+            val layout = mTextView.layout
+            if(layout != null){
+                val scrollDelta = (layout.getLineBottom( mTextView.lineCount -1)
+                        - mTextView.scrollY - mTextView.height)
+                if (scrollDelta > 0)
+                    mTextView.scrollBy( 0, scrollDelta)
+            }
+        }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(if (currentFocus == null) View( this) else currentFocus)
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
